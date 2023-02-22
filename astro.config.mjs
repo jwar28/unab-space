4115880619373e4da1eed10aca4fe64f2eb52dff
jwar28 <javierguerra2203@gmail.com>
2023-02-22 15:42:12 -0500

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+
+export default defineConfig({
+  site: "https://astroship.web3templates.com",
+  title: "Unabspace",
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    mdx(),
+    sitemap(),
+  ],
+});
